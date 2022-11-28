7b2f3b1326fe3c9d9a4cf3721eb6e2dd0d6083ad
ilsaf <justsairex@gmail.com>
2022-11-28 15:34:57 +0300

    add dynamic parsing tests from dropi.ru

--- parsing.py
+++ parsing.py
@@ -1,36 +1,23 @@
 from bs4 import BeautifulSoup as bs
-import codecs
+import requests
 from db import DBHelper
+import models
 
-file = 'parsing_file.html'
+URL = 'https://dropi.ru/....'
+# tests from https://dropi.ru/
 
-number_answer = 1
-questions = []
-answers_correct = []
-answers1 = []
-answers2 = []
-answers3 = []
-r = codecs.open(file, 'r', 'utf-8')
-soup = bs(r.read(), "html.parser")
-questions_pars = soup.find_all('h2')
-answers_correct_pars = soup.find_all(
-    class_='bTest__query__variant js-test-item-variant background--success results')
-answers_pars = soup.find_all(class_='bTest__query__variant js-test-item-variant results')
-for question in questions_pars:
-    questions.append(question.text)
-for answer_pars in answers_correct_pars:
-    answer_text = answer_pars.text[:-4]
-    answers_correct.append(answer_text.strip())
-for answer in answers_pars:
-    if number_answer == 1:
-        answer_text = answer.text[:-4]
-        answers1.append(answer_text.strip())
-        number_answer = 2
-    elif number_answer == 2:
-        answer_text = answer.text[:-4]
-        answers2.append(answer_text.strip())
-        number_answer = 3
-    elif number_answer == 3:
-        answer_text = answer.text[:-4]
-        answers3.append(answer_text.strip())
-        number_answer = 1
+r = requests.get(URL)
+soup = bs(r.text, "html.parser")
+db = DBHelper('db.file')
+
+for parent_block in soup.find_all(class_='bTest__query__item js-test-item'):
+    question = parent_block.find('h2')
+    db.session.add(models.Question(text=question.text, category_id=4))
+    question_id = db.session.query(models.Question).filter_by(text=question.text).first().id
+    answers_correct_block = parent_block.find_all('p')
+    for answer_block in answers_correct_block:
+        if answer_block['data-correct'] == 'true':
+            db.session.add(models.Answer(text=answer_block.text, is_correct=True, question_id=question_id))
+        elif answer_block['data-correct'] == 'false':
+            db.session.add(models.Answer(text=answer_block.text, is_correct=False, question_id=question_id))
+    db.session.commit()
