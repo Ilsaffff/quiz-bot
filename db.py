fbf7796ca6ff7db11fa9858265bb1d86edbc10c3
ilsaf <justsairex@gmail.com>
2022-12-07 23:53:04 +0300

    update method next_question, add_answer, add_user_category

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import User, Category, Question, Answer
+from models import User, Category, Question, Answer, UserAnswer
 from models import Base
 
 import random
@@ -35,13 +35,13 @@ class DBHelper:
     def add_user_answer(self, user_id, answer_id):
         user = self.session.query(User).filter_by(id=user_id).first()
         answer = self.session.query(Answer).filter_by(id=answer_id).first()
-        user.answers.append(answer)
+        self.session.add(UserAnswer(user_id=user.id, answer_id=answer.id))
         self.session.commit()
 
     def add_user_category(self, user_id, category_id):
         user = self.session.query(User).filter_by(id=user_id).first()
         category = self.session.query(Category).filter_by(id=category_id).first()
-        user.category = [category]
+        user.selected_category.append(category)
         self.session.commit()
 
     def get_user(self, user_id):
@@ -65,12 +65,8 @@ class DBHelper:
         question_id = self.session.query(Question).filter_by(text=question_text).first().id
         return question_id
 
-    def add_answer_true(self, answer_text, question_id):
-        self.session.add(Answer(text=answer_text, is_correct=True, question_id=question_id))
-        self.session.commit()
-
-    def add_answer_false(self, answer_text, question_id):
-        self.session.add(Answer(text=answer_text, is_correct=False, question_id=question_id))
+    def add_answer(self, answer_text, question_id, is_correct):
+        self.session.add(Answer(text=answer_text, is_correct=is_correct, question_id=question_id))
         self.session.commit()
 
     def get_question_by_id(self, question_id):
@@ -78,19 +74,8 @@ class DBHelper:
         return question
 
     def get_next_user_question(self, user_id, category_id):
-        questions_new = []
-        questions_user = []
         questions_all = self.session.query(Question).filter_by(category_id=category_id).all()
         answers_user = self.session.query(User).filter_by(id=user_id).first().answers
-        if answers_user:
-            for answer_user in answers_user:
-                questions_user.append(self.get_question_by_id(answer_user.question_id))
-            for question in questions_all:
-                if question in questions_user:
-                    continue
-                else:
-                    questions_new.append(question)
-        else:
-            questions_new = questions_all
-        question = random.choice(questions_new)
-        return question
+        questions_user = [self.get_question_by_id(answer_user.question_id) for answer_user in answers_user]
+        questions_new = list(set(questions_all).difference(set(questions_user)))
+        return random.choice(questions_new)
