af464d5d7ec4cd0aad6dd7be9d0c968a50099e5f
ilsaf <justsairex@gmail.com>
2022-12-07 23:54:33 +0300

    add parsing all tests from any category

--- parsing.py
+++ parsing.py
@@ -2,22 +2,28 @@ from bs4 import BeautifulSoup as bs
 import requests
 from db import DBHelper
 
-URL = 'https://dropi.ru/posts/test-na-proverku-nachitannosti-vechnyj-shedevry-smozhete-skazat-kto-avtor-etix-legendarnyx-proizvedenij'
-# tests from https://dropi.ru/
-category_id = 2
+db = DBHelper('foregun.db')
+MAIN_URL = 'https://dropi.ru'
+URL_TESTS = 'https://dropi.ru/c/worldhistory'
 
-r = requests.get(URL)
+category_id = 5
+
+r = requests.get(URL_TESTS)
 soup = bs(r.text, "html.parser")
-db = DBHelper('context.db')
 
-for parent_block in soup.find_all(class_='bTest__query__item js-test-item'):
-    question = parent_block.find('h2')
-    db.add_question(question_text=question.text, category_id=category_id)
-    question_id = db.get_question_id(question_text=question.text)
-    answers_block = parent_block.find_all(class_='bTest__query__variant js-test-item-variant')
-    for answer_block in answers_block:
-        if answer_block['data-correct'] == 'true':
-            db.add_answer_true(answer_text=answer_block.text, question_id=question_id)
-        elif answer_block['data-correct'] == 'false':
-            db.add_answer_false(answer_text=answer_block.text, question_id=question_id)
+href_all = soup.find('div', class_='grid__col grid__col--xs-12 grid__col--md-7 grid__col--lg-8 content__col').find_all(
+    'a', {'class': 'link link--text'})
+for href in href_all:
+    req = requests.get(MAIN_URL + href.get('href'))
+    soup = bs(req.text, "html.parser")
+    for parent_block in soup.find_all(class_='bTest__query__item js-test-item'):
+        question = parent_block.find('h2')
+        db.add_question(question_text=question.text, category_id=category_id)
+        question_id = db.get_question_id(question_text=question.text)
+        answers_block = parent_block.find_all(class_='bTest__query__variant js-test-item-variant')
+        for answer_block in answers_block:
+            if answer_block['data-correct'] == 'true':
+                db.add_answer(answer_text=answer_block.text, is_correct=True, question_id=question_id)
+            elif answer_block['data-correct'] == 'false':
+                db.add_answer(answer_text=answer_block.text, is_correct=False, question_id=question_id)
 print('Done!')
